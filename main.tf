68f7f2981a3c328cfc087d9160361d1c0aceb110
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-21 12:11:30 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -48,8 +48,8 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
   gateway_id = aws_internet_gateway.igw.id
   route = { [
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0",
+    gateway_id = aws_internet_gateway.igw.id,
   ] }
 }
 
