7259d2c9d0b4438cbed60cac3539d1ab10ec157b
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-21 11:26:09 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -68,56 +68,55 @@ resource "aws_route_table" "private" {
   }
 }
 
-resource "aws_route_table_association" "private" {
-  gateway_id = aws_nat_gateway.ngw.id
-  route_table_id = aws_route_table.private.id
-}
+# resource "aws_route_table_association" "private" {
+#   gateway_id = aws_nat_gateway.ngw.id
+#   route_table_id = aws_route_table.private.id
+# }
 
-data "aws_eks_cluster" "test_eks_cluster" {
-  name = module.test_eks_cluster.cluster_id
-}
+# data "aws_eks_cluster" "test_eks_cluster" {
+#   name = module.test_eks_cluster.cluster_id
+# }
 
-data "aws_eks_cluster_auth" "test_eks_cluster" {
-  name = module.test_eks_cluster.cluster_id
-}
+# data "aws_eks_cluster_auth" "test_eks_cluster" {
+#   name = module.test_eks_cluster.cluster_id
+# }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.9"
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   load_config_file       = false
+#   version                = "~> 1.9"
+# }
 
-module "test_eks_cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "test_eks_cluster"
-  cluster_version = "1.17"
-  subnets         = ["", ""]
-  vpc_id          = ""
+# module "test_eks_cluster" {
+#   source          = "terraform-aws-modules/eks/aws"
+#   cluster_name    = "test_eks_cluster"
+#   cluster_version = "1.17"
+#   subnets         = ["", ""]
+#   vpc_id          = ""
 
-node_groups = {
-  public = {
-    subnets          = [""]
-    desired_capacity = 1
-    max_capacity     = 10
-    min_capacity     = 1
+# node_groups = {
+#   public = {
+#     subnets          = [""]
+#     desired_capacity = 1
+#     max_capacity     = 10
+#     min_capacity     = 1
 
-    instance_type    = "${var.INSTANCE_TYPE}"
-    k8s_lables       = {
-      Environment    = "public"
-    }
-  }
- private = {
-    subnets          = [""]
-    desired_capacity = 1
-    max_capacity     = 10
-    min_capacity     = 1
+#     instance_type    = "${var.INSTANCE_TYPE}"
+#     k8s_lables       = {
+#       Environment    = "public"
+#     }
+#   }
+#  private = {
+#     subnets          = [""]
+#     desired_capacity = 1
+#     max_capacity     = 10
+#     min_capacity     = 1
 
-    instance_type    = "${var.INSTANCE_TYPE}"
-    k8s_lables       = {
-      Environment    = "private"
-    }
-  }
-}
-
+#     instance_type    = "${var.INSTANCE_TYPE}"
+#     k8s_lables       = {
+#       Environment    = "private"
+#     }
+#   }
+# }
